9e2e183643954f1fe205dae310c7c455527811c8
Thi Hue Le <ge35kax@mytum.de>
2024-01-12 16:30:21 +0100

    habit Tracking App

---
+++ day37_habitTrackingApp/README.md
@@ -0,0 +1,12 @@
+This app is used to track our habit
+
+* Create our profile in pixe.la
+* Create Graph 
+* update, change, delete the time or km or calories (depends on what we choose when we create our graph in graph_params variable)
+* For more info check in website: https://docs.pixe.la/
+
+*** 
+***
+*** For result, kindly check out in 
+https://pixe.la/v1/users/<username>/graphs/<graphid>.html
+* Please change <user name> and <graphid> to what you choose in code
---
+++ day37_habitTrackingApp/main1.py
@@ -0,0 +1,69 @@
+import requests
+from datetime import datetime
+
+# ----------------------------------------- CREATE OUR PROFILE IN PIXE.LA -----------------------------------------
+pixela_endpoint = "https://pixe.la/v1/users"
+
+# user token,user name, graphid  is chosen by us
+
+USER_TOKEN = "dnedkwnd943fnldfms"
+USER_NAME = "florianbaby"
+GRAPH_ID = "graphforflo"
+
+user_parameters = {
+    "token": USER_TOKEN,
+    "username": USER_NAME,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes"
+}
+
+# use this post 1 times to create new post profile for unexisting profile, then delete
+# response=requests.post(url=pixela_endpoint, json=user_parameters)
+# print(response.text)
+
+
+# ---------------------------------------------- CREATE GRAPH----------------------------------------------
+graph_endpoint = f"{pixela_endpoint}/{USER_NAME}/graphs"
+headers = {
+    "X-USER-TOKEN": USER_TOKEN
+}
+graph_params = {
+    "id": GRAPH_ID,
+    "name": "Eat tracking",
+    "unit": "calories",
+    "type": "float",
+    "color": "shibafu",
+}
+
+# use these 2 steps to create first unexisting graph, then delete when the graph sucessfully created
+# graph_response=requests.post(url=graph_endpoint, json=graph_params, headers=headers)
+# print(graph_response.text)
+
+
+# ---------------------------------------------- POST PIXEL, NEW VALUE ----------------------------------------------
+today = datetime.now().strftime("%Y%m%d")
+print(today)
+pixel_endpoint = f"{pixela_endpoint}/{USER_NAME}/graphs/{GRAPH_ID}"
+pixel_config = {
+    "date": today,
+    "quantity": "9.5"
+}
+
+# use this to add new value
+pixel_response = requests.post(url=pixel_endpoint, json=pixel_config, headers=headers)
+# check picel rsponse
+# print(pixel_response.text)
+
+# ---------------------------------------------- UPDATE PIXEL, VALUE----------------------------------------------
+pixel_update_endpoint = f"{pixela_endpoint}/{USER_NAME}/graphs/{GRAPH_ID}/{today}"
+pixel_update_config = {
+    "quantity": "7.5"
+}
+# pixel_update_response = requests.put(url=pixel_update_endpoint, json=pixel_update_config, headers=headers)
+# print(pixel_update_response.text)
+
+# ---------------------------------------------- DELETE PIXEL, VALUE----------------------------------------------
+
+delete_endpoint = f"{pixela_endpoint}/{USER_NAME}/graphs/{GRAPH_ID}/{today}"
+# delete_response = requests.delete(url=delete_endpoint, headers=headers)
+# print(delete_response)
